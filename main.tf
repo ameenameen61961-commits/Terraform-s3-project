2b6d6350d3a0a24e709a8e6ed32c04c96196c9a4
Ahmed Al Ameen <ameenameen61961@gmail.com>
2026-09-05 13:06:40 +0530

    Add EC2 instance and S3 bucket resources
    Added AWS EC2 instance and S3 bucket resources with tags.

--- main.tf
+++ main.tf
@@ -1,11 +1,28 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 }
 
 provider "aws" {
   region = "ap-south-1"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
+
+resource "aws_s3_bucket" "storage" {
+  bucket = "alameen-terraform-s3-project-2026"
+
+  tags = {
+    Name = "Terraform-S3"
+  }
 }
